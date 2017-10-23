58d94b1f75bbc2c88531ccf2b7ab4410e68b7077
John Roberts <john.calvin.roberts@gmail.com>
2017-10-23 16:26:47 +0800

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'dao_test',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    // vendor: [ 'element-ui' ], if include vendor it will be compiled into a static file
+    babel: {
+      "presets": [
+        ["env", {
+          "modules": false,
+          "targets": {
+            "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
+          }
+        }],
+        "stage-2"
+      ],
+      "plugins": [
+        "transform-runtime",
+        ["component", [
+          {
+            "libraryName": "element-ui",
+            "styleLibraryName": "../../../node_modules/element-theme-heyshop/theme"
+          }
+      ]]]
+    }
+  },
+  plugins: [
+    { src: '~/plugins/element-ui', ssr: true }
+  ]
+}
---
+++ plugins/README.md
@@ -0,0 +1,3 @@
+# Element-ui plugin usage
+
+You can remove any component as you need.
